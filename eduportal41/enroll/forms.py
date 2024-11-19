785ffc344bdf8301b30831ed81124ea1a1936e86
Foram Patel <foramdelvadiya@gmail.com>
2024-11-19 15:25:17 +0530

    Add files via upload

---
+++ eduportal41/enroll/forms.py
@@ -0,0 +1,18 @@
+from typing import Any
+from django import forms
+
+class StudentRegistration(forms.Form):
+    name = forms.CharField()
+    email = forms.EmailField()
+    
+    def clean(self):
+        clean_data = super().clean()
+        vlname = self.cleaned_data['name']
+        vlemail = self.cleaned_data['email']
+
+        if len(vlname) < 8 :
+            raise forms.ValidationError('Name should be more than or equal 8 character ')
+        
+        if len(vlemail) < 20 :
+            raise forms.ValidationError('Email should be more than or equal 20 character')
+    
